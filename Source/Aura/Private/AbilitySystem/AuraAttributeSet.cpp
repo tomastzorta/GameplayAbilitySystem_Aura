8b1ee247598ab331058e7bc34551550a8bd1cef9
Tomas Tzorta <greekharvarddev@gmail.com>
2023-10-24 02:08:29 +0100

    75. Initializing Vital Attributes

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -10,8 +10,7 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-	InitHealth(50.f);
-	InitMana(150.f);
+	
 }
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
