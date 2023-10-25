e35849e65efafede87d58dd1b694f4441b16c118
Tomas Tzorta <greekharvarddev@gmail.com>
2023-10-25 08:42:26 +0100

    94. Widget Attribute Tags + Improved Solution

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,12 +3,44 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
-	
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	check(AttributeInfo);
+
+	for (auto& Tag : AttributeInfo.Get()->AttributeInformation)
+	{
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Tag.AttributeGetter).AddLambda([this, Tag](const FOnAttributeChangeData& Data)
+		{
+			BroadcastAttributeInfo(Tag.AttributeTag);
+		});
+	}
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	//the DataAsset
+	check(AttributeInfo);
+
+	for (auto& Tag : AttributeInfo.Get()->AttributeInformation)
+	{
+		BroadcastAttributeInfo(Tag.AttributeTag);
+	}
+}
+
+void UAttributeMenuWidgetController::BroadcastAttributeInfo(const FGameplayTag& AttributeTag) const
+{
+	//getting info from DataAsset based on the gameplay tag match.
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(AttributeTag);
+	//Set the hidden attribute value in the dataset.
+	Info.AttributeValue = Info.AttributeGetter.GetNumericValue(AttributeSet);
+	//broadcast for those who subscribe. IE the attribute menu widget in blueprint
+	AttributeInfoDelegate.Broadcast(Info);
 	
 }
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -5,7 +5,6 @@
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
 #include "AbilitySystemComponent.h"
-#include "GameplayEffectExtension.h"
 #include "AuraAttributeSet.generated.h"
 
 #define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
