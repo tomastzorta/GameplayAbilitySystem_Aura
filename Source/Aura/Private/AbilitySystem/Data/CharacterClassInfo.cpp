4e3320212b18a53f040b10607124275e4ecaa61e
Tomas Tzorta <greekharvarddev@gmail.com>
2023-10-30 00:09:19 +0000

    128. Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// CopyrightTrojanEngine
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
